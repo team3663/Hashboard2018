d585f1289c625ca39f081bb34a73e61904ae7f7a
Trent Hashimoto <t.hashiphoto@gmail.com>
2018-04-16 18:10:07 -0700

    Added a .gitignore file to ignore classpath differnces

--- src/controlpanel/MyButton.java
+++ src/controlpanel/MyButton.java
@@ -6,11 +6,13 @@ import javax.swing.JButton;
 public class MyButton extends JButton {
 	public int index;
 	public String displayName;
+	public boolean isPressed;
 	public MyButton() {
 		super();
 	}
 	public MyButton(String name) {
 		super(name);
 		displayName = name;
+		isPressed = false;
 	}
 }
